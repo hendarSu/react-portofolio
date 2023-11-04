fd105ce60d81ca9df67a68f245826952a91fb96d
Hendar <hendarsundana@gmail.com>
2023-11-04 07:36:35 +0700

    feat: state

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,22 +1,25 @@
+import { useEffect, useState } from "react";
 import FeatureItem from "./FeatureItem";
+import featuresData from "@/utils/data/features.json";
 
 const Feature = () => {
+  const [features, setFeatures] = useState([]);
+
+  useEffect(() => {
+    setFeatures(featuresData);
+  }, []);
+
   return (
     <section>
       <hr />
       <div className="row">
-        <div className="col-md-3">
-          <FeatureItem></FeatureItem>
-        </div>
-        <div className="col-md-3">
-          <FeatureItem></FeatureItem>
-        </div>
-        <div className="col-md-3">
-          <FeatureItem></FeatureItem>
-        </div>
-        <div className="col-md-3">
-          <FeatureItem></FeatureItem>
-        </div>
+        {features.map((data, index) => {
+          return (
+            <div className="col-md-3" key={index}>
+              <FeatureItem></FeatureItem>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
--- src/components/FeatureItem.jsx
+++ src/components/FeatureItem.jsx
@@ -2,7 +2,7 @@ import { Image } from "react-bootstrap";
 
 const FeatureItem = () => {
     return (
-        <div className="card">
+        <div className="card mb-2">
             <Image src="https://img.freepik.com/free-photo/happy-teen-girl-playing-baseball_23-2148209546.jpg?w=2000&t=st=1698931851~exp=1698932451~hmac=342872d023a09eac8def652c90517e72776ec15430bd21306496db7a7e191b8a" className="card-img-top"/>
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
